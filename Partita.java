ee6f7d84d884a76baf29fc69e0918c79e5760187
Danyeleee <105109977+Danyeleee@users.noreply.github.com>
2022-05-07 17:36:21 +0200

    Update Partita.java

--- Partita.java
+++ Partita.java
@@ -6,11 +6,11 @@ public class Partita{
 		ArrayList<Player> giocatori= new ArrayList<Player>();
 		
 		System.out.println("Inserisci il nome del primo giocatore:");
-		Player g1= new Player(in.next(),0);
+		Player g1= new Player(in.next(),1);
 		giocatori.add(g1);
 		
 		System.out.println("Inserisci il nome del secondo giocatore");
-		Player g2= new Player(in.next(),1);
+		Player g2= new Player(in.next(),2);
 		giocatori.add(g2);
 		
 		Griglia grill= new Griglia();
@@ -21,7 +21,7 @@ public class Partita{
 		boolean check= false;
 		int turno=0;
 		while (!check) {
-			System.out.print(giocatori.get(turno%2)+" fai la tua mossa, scegli la colonna(tra 0 e 6) in cui inserire la pedina");
+			System.out.print(giocatori.get(turno%2).getName()+" fai la tua mossa, scegli la colonna(tra 0 e 6) in cui inserire la pedina");
 			check= game.addMove(in.nextInt(), giocatori.get(turno%2).getNumber(), turno);
 			
 		}
