b495fcca405a311dae5a6d41a1ffd40042d50bee
Danyeleee <105109977+Danyeleee@users.noreply.github.com>
2022-06-03 12:26:38 +0200

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,21 +1,21 @@
 /**
-  	A person is represented by the name.
+      A person is represented by the name.
   */
 public class Person {
 
 	private String name;
 	
 	/**
-    	Construct a Person object.
-    	@param n the name of the person
-    */
+    	    Construct a Person object.
+    	    @param n the name of the person
+        */
 	public Person(String n) {
 		this.name=n;
 	}
 	
 	/**
-		Get the name of the person.
-		@return name of the person
+	    Get the name of the person.
+	    @return name of the person
 	 */
 	public String getName() {
 		return name;
